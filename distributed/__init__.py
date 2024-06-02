2d9a7837c8550b5da669b68895fbac45a4aae15d
Cow-Kite <bbsr0819@gmail.com>
2024-06-02 21:49:12 +0900

    commit

--- distributed/__init__.py
+++ distributed/__init__.py
@@ -1,17 +1,7 @@
-from .dist_context import DistContext
 from .local_feature_store import LocalFeatureStore
 from .local_graph_store import LocalGraphStore
-from .dist_neighbor_sampler import DistNeighborSampler
-from .dist_loader import DistLoader
-from .dist_neighbor_loader import DistNeighborLoader
-from .toy_sampler import ToySampler
 
 __all__ = classes = [
-    'DistContext',
     'LocalFeatureStore',
     'LocalGraphStore',
-    'DistNeighborSampelr',
-    'DistLoader',
-    'DistNeighborLoader',
-    'ToySampler'
 ]
