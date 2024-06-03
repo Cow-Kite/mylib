d7b5e42a464cee1592c41d6c781bb53ef0fea4a5
Cow-Kite <bbsr0819@gmail.com>
2024-06-03 15:22:47 +0900

    24.06.03

--- distributed/__init__.py
+++ distributed/__init__.py
@@ -1,7 +1,11 @@
+from .dist_context import DistContext
 from .local_feature_store import LocalFeatureStore
 from .local_graph_store import LocalGraphStore
+from .dist_neighbor_sampler import DistNeighborSampler
 
 __all__ = classes = [
     'LocalFeatureStore',
     'LocalGraphStore',
+    'DistContext',
+    'DistNeighborSampler',
 ]
