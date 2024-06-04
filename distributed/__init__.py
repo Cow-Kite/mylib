65c5263f7a1253c222d6349eea6efb24042a41ad
Cow-Kite <bbsr0819@gmail.com>
2024-06-04 17:02:49 +0900

    24.06.04 update

--- distributed/__init__.py
+++ distributed/__init__.py
@@ -2,10 +2,12 @@ from .dist_context import DistContext
 from .local_feature_store import LocalFeatureStore
 from .local_graph_store import LocalGraphStore
 from .dist_neighbor_sampler import DistNeighborSampler
+from .dist_neighbor_loader import DistNeighborLoader
 
 __all__ = classes = [
     'LocalFeatureStore',
     'LocalGraphStore',
     'DistContext',
     'DistNeighborSampler',
+    'DistNeighborLoader',
 ]
--- test.py
+++ test.py
@@ -1,67 +1,73 @@
-import sys
+import argparse
 import os.path as osp
-import torch
-from multiprocessing import Process, Queue
-from distributed import DistContext
-from distributed import LocalGraphStore
-from distributed import LocalFeatureStore
-from distributed import DistNeighborSampler
+import time
+from contextlib import nullcontext
 
-def process_data(node_rank, dataset, dataset_root_dir):
-    print(f'--- Process {node_rank}: Loading data partition files ...')
+import torch
+import torch.distributed
+import torch.nn.functional as F
+from torch.nn.parallel import DistributedDataParallel
+from tqdm import tqdm
+
+from torch_geometric.data import HeteroData
+from .distributed import (
+    DistContext,
+    DistNeighborLoader,
+    LocalFeatureStore,
+    LocalGraphStore,
+)
+from torch_geometric.nn import GraphSAGE, to_hetero
+
+
+
+def run_proc(
+    local_proc_rank: int,
+    num_nodes: int,
+    node_rank: int,
+    dataset: str,
+    dataset_root_dir: str,
+    master_addr: str,
+
+):
+    is_hetero = dataset == 'ogbn-mag'
+
+    print('--- Loading data partition files ...')
     root_dir = osp.join(osp.dirname(osp.realpath(__file__)), dataset_root_dir)
     node_label_file = osp.join(root_dir, f'{dataset}-label', 'label.pt')
-
     train_idx = torch.load(
         osp.join(
             root_dir,
             f'{dataset}-train-partitions',
             f'partition{node_rank}.pt',
         ))
-    
     test_idx = torch.load(
-            osp.join(
-                    root_dir,
-                        f'{dataset}-test-partitions',
-                        f'partition{node_rank}.pt'
-            ))
+        osp.join(
+            root_dir,
+            f'{dataset}-test-partitions',
+            f'partition{node_rank}.pt',
+        ))
     
+    if is_hetero:
+        train_idx = ('paper', train_idx)
+        test_idx = ('paper', test_idx)
+
+    # Load partition into local graph/feature store:
     graph = LocalGraphStore.from_partition(
         osp.join(root_dir, f'{dataset}-partitions'), node_rank)
-
     feature = LocalFeatureStore.from_partition(
         osp.join(root_dir, f'{dataset}-partitions'), node_rank)
-
     feature.labels = torch.load(node_label_file)
     partition_data = (feature, graph)
+    print(f'Partition metadata: {graph.meta}')
 
-    print(f'Partition metadata (node_rank {node_rank}): {graph.meta}')
-
-    current_ctx = DistContext()
-    num_neighbors = [10, 5]
-    sampler = DistNeighborSampler(
-        current_ctx=current_ctx,
-        data=partition_data,
-        num_neighbors=num_neighbors,
-        channel=queue,
-        device=torch.device('cpu')
+    # Initialize distributed context:
+    current_ctx = DistContext(
+        world_size=num_nodes,
+        rank=node_rank,
+        global_world_size=num_nodes,
+        global_rank=node_rank,
+        group_name="distributed-ogb-sage",
     )
-    sampler.init_sampler_instance()
+    current_ctx = torch.device('cpu')
 
     
-def main():
-    dataset = 'ogbn-products'
-    dataset_root_dir = './data/partitions/ogbn-products/2-parts'
-
-    processes = []
-    for node_rank in range(2):
-        p = Process(target=process_data, args=(node_rank, dataset, dataset_root_dir))
-        p.start()
-        processes.append(p)
-
-    for p in processes:
-        p.join()
-
-if __name__ == "__main__":
-    main()
-
